fa3dfeb1acd71846b90988f5a74c14441612b927
Giau <giaunv@gmail.com>
2015-03-14 15:41:07 +0700

    Adding Airplane Animations

--- airplane/GameScene.h
+++ airplane/GameScene.h
@@ -7,13 +7,18 @@
 //
 
 #import <SpriteKit/SpriteKit.h>
+#import <CoreMotion/CoreMotion.h>
 
 @interface GameScene : SKScene{
     CGRect screenRect;
     CGFloat screenHeight;
     CGFloat screenWidth;
+    double currentMaxAccelX;
+    double currentMaxAccelY;
 }
 
+@property (strong, atomic) CMMotionManager *motionManager;
 @property SKSpriteNode *plane;
-
+@property SKSpriteNode *planeShadow;
+@property SKSpriteNode *propeller;
 @end
--- airplane/GameScene.m
+++ airplane/GameScene.m
@@ -10,19 +10,6 @@
 
 @implementation GameScene
 
-/*
--(void)didMoveToView:(SKView *)view {
-    /* Setup your scene here
-    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-    
-    myLabel.text = @"Hello, World!";
-    myLabel.fontSize = 65;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
-    
-    [self addChild:myLabel];
-}*/
-
 -(id)initWithSize:(CGSize)size{
     if (self = [super initWithSize:size]) {
         // init several sizes used in all scene
@@ -37,11 +24,39 @@
         _plane.position = CGPointMake(screenWidth/2, 15 + _plane.size.height/2);
         [self addChild:_plane];
         
+        _planeShadow = [SKSpriteNode spriteNodeWithImageNamed:@"PLANE 8 SHADOW.png"];
+        _planeShadow.scale = 0.6;
+        _planeShadow.zPosition = 1;
+        _planeShadow.position = CGPointMake(screenWidth/2 + 15, 0 + _planeShadow.size.height/2);
+        [self addChild:_planeShadow];
+        
+        _propeller = [SKSpriteNode spriteNodeWithImageNamed:@"PLANE PROPELLER 1.png"];
+        _propeller.scale = 0.2;
+        _propeller.position = CGPointMake(screenWidth/2, _plane.size.height+10);
+        
+        SKTexture *propeller1 = [SKTexture textureWithImageNamed:@"PLANE PROPELLER 1.png"];
+        SKTexture *propeller2 = [SKTexture textureWithImageNamed:@"PLANE PROPELLER 2.png"];
+        
+        SKAction *spin = [SKAction animateWithTextures:@[propeller1, propeller2] timePerFrame:0.1];
+        SKAction *spinForever = [SKAction repeatActionForever:spin];
+        [_propeller runAction:spinForever];
+        [self addChild:_propeller];
         
         //adding the background
         SKSpriteNode *background = [SKSpriteNode spriteNodeWithImageNamed:@"airPlanesBackground"];
         background.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
         [self addChild:background];
+        
+        //CoreMotion
+        self.motionManager = [[CMMotionManager alloc] init];
+        self.motionManager.accelerometerUpdateInterval = .2;
+        
+        [self.motionManager startAccelerometerUpdatesToQueue:[NSOperationQueue currentQueue] withHandler:^(CMAccelerometerData *accelerometerData, NSError *error) {
+            [self outputAccelertionData:accelerometerData.acceleration];
+            if (error) {
+                NSLog(@"%@", error);
+            }
+        }];
     }
     
     return self;
@@ -49,12 +64,22 @@
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
-    
-    
 }
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
+}
+
+-(void)outputAccelertionData:(CMAcceleration)acceleration{
+    currentMaxAccelX = 0;
+    currentMaxAccelY = 0;
+    
+    if (fabs(acceleration.x) > fabs(currentMaxAccelX)) {
+        currentMaxAccelX = acceleration.x;
+    }
+    if (fabs(acceleration.y) > fabs(currentMaxAccelY)) {
+        currentMaxAccelY = acceleration.y;
+    }
 }
 
 @end
